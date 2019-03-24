c3d3595c55878400d57275014ae3d39de84e738d
trick77 <trick77@users.noreply.github.com>
2019-03-24 07:26:42 +0100

    Update import.sh

--- import.sh
+++ import.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
-cli="/usr/sbin/asterisk -rx"
-dump=/tmp/blacklist.backup
-blacklist=/tmp/latest_cc_blacklist.txt
- 
-wget -O${blacklist} https://raw.githubusercontent.com/trick77/callcenter-blacklist-switzerland/master/latest_cc_blacklist.txt
- 
-echo "Dumping old blacklist to $dump"
-$cli "database show blacklist" &amp;gt; $dump
- 
+CLI="/usr/sbin/asterisk -rx"
+TMPFILE=$(mktemp /tmp/blacklist.XXXXXX)
+
+curl -s -o ${TMPFILE} https://raw.githubusercontent.com/trick77/callcenter-blacklist-switzerland/master/latest_cc_blacklist.txt
+# Add static custom entries with
+#cat /etc/callcenter-blacklist/custom-blacklist.txt >> $TMPFILE
+
 if [ "$1" == "-d" ]; then
-  echo "Removing all existing blacklist entries"
-  $cli "database deltree blacklist"
+  echo "Removing all existing blacklist entries..."
+  ${CLI}i "database deltree blacklist"
 fi
- 
-cat $blacklist | while read line; do
-  [[ $line = \#* ]] &amp;amp;&amp;amp; continue
-  number=${line%;*}
-  desc=${line#*;}
-  echo "database put blacklist $number \"$desc\""
-  $cli "database put blacklist $number \"$desc\""
+
+echo "Importing blacklist data..."
+cat ${TMPFILE} | while read line; do
+  [[ ${line} = \#* ]] && continue
+  NUMBER=${line%;*}
+  DESC=${line#*;}
+  echo "database put blacklist ${NUMBER} \"${DESC}\""
+  ${CLI} "database put blacklist ${NUMBER} \"${DESC}\""
 done
+
+rm ${TMPFILE}
